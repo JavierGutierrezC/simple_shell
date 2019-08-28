95be38709e3bac61d6b8126b9bb8116413e5e9b0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-28 20:18:04 +0000

    [fix]free

--- mishell.c
+++ mishell.c
@@ -28,22 +28,25 @@ int main(int ac, char *av[], char **environ)
 		if (characters == EOF)
 		{
 			free(bf);
-			exit(0);
-		}
+			exit(0); }
 
-		if (*bf != '\n' && (_strcmp(bf, "env\n") != 0))
+		if (*bf != '\n')
 		{
 			bftoken = linetoken(bf);
-			if (_strcmp(bftoken[0], "\n") != 0)
+			if ((_strcmp(bftoken[0], "\n") != 0) && (_strcmp(bftoken[0], "env") != 0))
 			{
-				pat = findpath(environ);
-				goesin = _stat(bftoken, pat);
-				exefork(bftoken, goesin, environ);
+				if ((_strcmp(bftoken[0], "env") != 0))
+				{
+					find_env(environ); }
+				else
+				{
+					pat = findpath(environ);
+					goesin = _stat(bftoken, pat);
+					exefork(bftoken, goesin, environ); }
 			}
-		}
-		if ((_strcmp("env\n", bf)) == 0)
-		{
-			find_env(environ);
+			else
+			{
+				free(bftoken); }
 		}
 	}
 	return (0);
